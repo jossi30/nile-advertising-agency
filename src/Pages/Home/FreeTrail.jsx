eb2ce14652fce227f8031b82cb48f7a761209546
Yosief G. Hailay <yosiefghaylay@gmail.com>
2024-06-10 21:34:59 +0300

    hidden the form section

--- src/Pages/Home/FreeTrail.jsx
+++ src/Pages/Home/FreeTrail.jsx
@@ -10,6 +10,7 @@ export default function ContactMe() {
     message: '',
   });
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -40,113 +41,127 @@ export default function ContactMe() {
       });
   };
 
+  const handleShowForm = () => {
+    setIsFormVisible(true);
+  };
+
   if (isSubmitted) {
-    return <p className='thank-u'>Thank you , we will be contacting you shortly</p>;
+    return <p className='thank-u'>Thank you, we will be contacting you shortly</p>;
   }
 
   return (
     <section id="free-trail" className="contact--section">
       <div>
-        <p className="sub--title">Fill the form to get a</p>
-        <h2>Free Trail</h2>
-        <p className="text-lg">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, odit.
-        </p>
+        {!isFormVisible && (
+          <button onClick={handleShowForm} className="btn btn-github">
+            Click Here to Sign Up for a free trail
+          </button>
+        )}
+        {isFormVisible && (
+          <>
+            <div>
+              <p className="sub--title">Fill the form to get a</p>
+              <h2>Free Trail</h2>
+              <p className="text-lg">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, odit.
+              </p>
+            </div>
+            <form className="contact--form--container" onSubmit={handleSubmit}>
+              <div className="container">
+                <label htmlFor="firstName" className="contact--label">
+                  <span className="text-md">First Name</span>
+                  <input
+                    type="text"
+                    className="contact--input text-md"
+                    name="firstName"
+                    id="firstName"
+                    value={formState.firstName}
+                    onChange={handleChange}
+                    required
+                  />
+                </label>
+                <label htmlFor="lastName" className="contact--label">
+                  <span className="text-md">Last Name</span>
+                  <input
+                    type="text"
+                    className="contact--input text-md"
+                    name="lastName"
+                    id="lastName"
+                    value={formState.lastName}
+                    onChange={handleChange}
+                    required
+                  />
+                </label>
+                <label htmlFor="email" className="contact--label">
+                  <span className="text-md">Email</span>
+                  <input
+                    type="email"
+                    className="contact--input text-md"
+                    name="email"
+                    id="email"
+                    value={formState.email}
+                    onChange={handleChange}
+                    required
+                  />
+                </label>
+                <label htmlFor="phoneNumber" className="contact--label">
+                  <span className="text-md">Phone Number</span>
+                  <input
+                    type="number"
+                    className="contact--input text-md"
+                    name="phoneNumber"
+                    id="phoneNumber"
+                    value={formState.phoneNumber}
+                    onChange={handleChange}
+                    required
+                  />
+                </label>
+              </div>
+              <label htmlFor="topic" className="contact--label">
+                <span className="text-md">Choose a topic</span>
+                <select
+                  id="topic"
+                  className="contact--input text-md"
+                  name="topic"
+                  value={formState.topic}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="" disabled>Select One...</option>
+                  <option>Seo</option>
+                  <option>Email Marketing</option>
+                  <option>Brand Marketing</option>
+                </select>
+              </label>
+              <label htmlFor="message" className="contact--label">
+                <span className="text-md">Message</span>
+                <textarea
+                  className="contact--input text-md"
+                  id="message"
+                  name="message"
+                  rows="8"
+                  placeholder="Type your message..."
+                  value={formState.message}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+              <label htmlFor="checkbox" className="checkbox--label">
+                <input
+                  type="checkbox"
+                  required
+                  name="checkbox"
+                  id="checkbox"
+                />
+                <span className="text-sm">I accept the terms</span>
+              </label>
+              <div>
+                <button className="btn btn-primary contact--form--btn">Submit</button>
+              </div>
+            </form>
+          </>
+        )}
       </div>
-      <form className="contact--form--container" onSubmit={handleSubmit}>
-        <div className="container">
-          <label htmlFor="firstName" className="contact--label">
-            <span className="text-md">First Name</span>
-            <input
-              type="text"
-              className="contact--input text-md"
-              name="firstName"
-              id="firstName"
-              value={formState.firstName}
-              onChange={handleChange}
-              required
-            />
-          </label>
-          <label htmlFor="lastName" className="contact--label">
-            <span className="text-md">Last Name</span>
-            <input
-              type="text"
-              className="contact--input text-md"
-              name="lastName"
-              id="lastName"
-              value={formState.lastName}
-              onChange={handleChange}
-              required
-            />
-          </label>
-          <label htmlFor="email" className="contact--label">
-            <span className="text-md">Email</span>
-            <input
-              type="email"
-              className="contact--input text-md"
-              name="email"
-              id="email"
-              value={formState.email}
-              onChange={handleChange}
-              required
-            />
-          </label>
-          <label htmlFor="phoneNumber" className="contact--label">
-            <span className="text-md">Phone Number</span>
-            <input
-              type="number"
-              className="contact--input text-md"
-              name="phoneNumber"
-              id="phoneNumber"
-              value={formState.phoneNumber}
-              onChange={handleChange}
-              required
-            />
-          </label>
-        </div>
-        <label htmlFor="topic" className="contact--label">
-          <span className="text-md">Choose a topic</span>
-          <select
-            id="topic"
-            className="contact--input text-md"
-            name="topic"
-            value={formState.topic}
-            onChange={handleChange}
-            required
-          >
-            <option value="" disabled>Select One...</option>
-            <option>Seo</option>
-            <option>Email Marketing</option>
-            <option>Brand Marketing</option>
-            
-          </select>
-        </label>
-        <label htmlFor="message" className="contact--label">
-          <span className="text-md">Message</span>
-          <textarea
-            className="contact--input text-md"
-            id="message"
-            name="message"
-            rows="8"
-            placeholder="Type your message..."
-            value={formState.message}
-            onChange={handleChange}
-            required
-          />
-        </label>
-        <label htmlFor="checkbox" className="checkbox--label">
-          <input
-            type="checkbox"
-            required
-            name="checkbox"
-            id="checkbox"
-          />
-          <span className="text-sm">I accept the terms</span>
-        </label>
-        <div>
-          <button className="btn btn-primary contact--form--btn">Submit</button>
-        </div>
-      </form>
     </section>
   );
 }
