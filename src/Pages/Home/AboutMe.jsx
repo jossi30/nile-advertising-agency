9dd53d283b28883d9c4e7f6091385d73e1803042
Yosief G. Hailay <yosiefghaylay@gmail.com>
2024-06-13 22:51:24 +0300

    made changes to the About Us section

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -9,15 +9,10 @@ export default function AboutMe() {
           <p className="section--title">Who We Are?</p>
           <h1 className="skills-section--heading">About Us</h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-            tempora numquam sint odit optio.
+          At Nile Marketing Agency, we specialize in helping small service-based businesses thrive in the competitive digital landscape. Our comprehensive suite of digital marketing services is designed to drive growth, enhance online presence, and deliver measurable results.   
           </p>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae?
+          We understand the unique challenges faced by small businesses and are committed to providing tailored solutions that meet your specific needs.
           </p>
         </div>
       </div>
